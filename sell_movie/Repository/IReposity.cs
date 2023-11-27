b13eec73b350f6e9b87b7175436ac8fd556b3f1b
Vinh Tien <phanvinhtien1712@gmail.com>
2023-11-27 10:49:06 +0700

    themrepo

---
+++ sell_movie/Repository/IReposity.cs
@@ -0,0 +1,11 @@
+﻿namespace sell_movie.Repository
+{
+    public interface IReposity<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(string id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
